d9c8c7c6f7ec0b4663536a7f1091664198945fc2
Kostya Grishchenko <kgrischenko@geeksforless.net>
2015-12-23 13:55:09 +0200

    Initial Commit

---
+++ MMChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MMChat
+//
+//  Created by Kostya Grishchenko on 12/23/15.
+//  Copyright © 2015 Kostya Grishchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
